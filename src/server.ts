a9df167ac5863846376cdb2a1967ea5f2d115894
LUIS ROBERTO OLIVEIRA SANTOS <luis.oliveira@equatorialenergia.com.br>
2022-04-08 17:13:59 -0300

    add /api

--- src/server.ts
+++ src/server.ts
@@ -17,8 +17,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(routes);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/api', routes);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
